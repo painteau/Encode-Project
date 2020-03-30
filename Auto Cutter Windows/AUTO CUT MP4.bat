cb15ed5e4880931ab3a66d4bb9bf44b19e061176
painteau <52933590+painteau@users.noreply.github.com>
2020-03-30 20:52:13 +0200

    First Commit

---
+++ Auto Cutter Windows/AUTO CUT MP4.bat
@@ -0,0 +1,2 @@
+ffmpeg -ss 00:2:41.0 -i input.mp4 -t 00:00:30.0 -vcodec copy -acodec copy output.mp4
+:: ffmpeg --ss START_TIME_HH:MIN:SEC.FRAME -i INPUT_FILE.mp4 -t DURATION_TIME_HH:MIN:SEC.FRAME -vcodec copy -acodec copy OUTPUT_FILE.mp4
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Encode-Project
  A project about how Encoder really work :D !
+ 
+ ## Features
+* All Original Files are captured in Full HD (1080p60) on OBS with x264 software encoding (CRF 5 / faster / High)
